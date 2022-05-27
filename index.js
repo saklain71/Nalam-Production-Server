d0bea7eececf79a7ad5c3c334f8b037cb22fbaf3
Saklain Mustak <saklainmustak@sakmac.local>
2022-05-27 22:08:10 +0600

    all tools intalled with middleware

--- index.js
+++ index.js
@@ -1,6 +1,14 @@
 const express = require('express')
 const app = express()
 const port = process.env.PORT ||  5000
+const dotenv = require('dotenv')
+const cors = require('cors')
+const jwt = require('jsonwebtoken');
+
+
+app.use(cors())
+app.use(express.json())
+
 
 app.get('/', (req, res) => {
   res.send('Welcome to Nalam tools Server')
