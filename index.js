530917f6148183cbb0a856febb614ff338e8fe7c
Saklain Mustak <saklainmustak@sakmac.local>
2022-05-28 00:22:07 +0600

    mongo connected

--- index.js
+++ index.js
@@ -10,6 +10,19 @@ app.use(cors())
 app.use(express.json())
 
 
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.imwsu.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+client.connect(err => {
+  const collection = client.db("nalamdb").collection("products");
+  // perform actions on the collection object
+  console.log('connected DB')
+  client.close();
+
+});
+
+
+
 app.get('/', (req, res) => {
   res.send('Welcome to Nalam tools Server')
 })
